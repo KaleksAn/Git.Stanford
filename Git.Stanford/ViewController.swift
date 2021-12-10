028e503db320a7538ac3d4e3304a2c6092d37d5f
Alex Kan <alx.kan037@gmail.com>
2021-12-10 22:42:14 +0500

    Revert

--- Git.Stanford/ViewController.swift
+++ Git.Stanford/ViewController.swift
@@ -21,12 +21,8 @@ class ViewController: UIViewController {
     }
     
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        print("viewDidAppear")
-    }
-    
     //First comment
+    
     func simpleFunc() {
         print("Simple function")
         print("Recovery function")
